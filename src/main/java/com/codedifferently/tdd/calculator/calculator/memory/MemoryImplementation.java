c82bc378264a2a7b5989fb48029598fd5c611848
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2020-11-17 21:09:30 -0500

    worked on memoryimplementation

--- src/main/java/com/codedifferently/tdd/calculator/calculator/memory/MemoryImplementation.java
+++ src/main/java/com/codedifferently/tdd/calculator/calculator/memory/MemoryImplementation.java
@@ -3,22 +3,25 @@ package com.codedifferently.tdd.calculator.calculator.memory;
 public class MemoryImplementation implements Memory{
     private Double currentValue;
 
+
     public MemoryImplementation(){
+
         this.currentValue = 0.0;
     }
 
     @Override
     public void addToCurrentValue(Double input) {
-
+        currentValue +=input;
     }
 
     @Override
     public void resetMemory() {
-
+        currentValue=0.0;
     }
 
     @Override
     public Double recallCurrentValue() {
-        return null;
+
+        return currentValue;
     }
 }
